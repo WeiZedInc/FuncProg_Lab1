69b5066046e89a4da45d1315c4153b3be29f4efb
Yurii Kyrpotenko <50027889+WeiZedInc@users.noreply.github.com>
2024-09-25 22:56:53 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using FuncProg_Lab1.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAntiforgery();
+
+app.MapGet("/products/{productId:int}", (int productId) =>
+{
+    var result = new
+    {
+        id = productId.ToString(),
+        name = $"{productId} name"
+    };
+    return Results.Json(result);
+});
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
